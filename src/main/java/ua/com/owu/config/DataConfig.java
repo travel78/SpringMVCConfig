9347b38989ae157dc6f97595610293be27215458
sergeyokten <sergey.okten@gmail.com>
2017-06-16 15:23:05 +0300

    1 service 2 path variable 3.SF 4. editor and validator 5.hikariCP

--- src/main/java/ua/com/owu/config/DataConfig.java
+++ src/main/java/ua/com/owu/config/DataConfig.java
@@ -1,9 +1,10 @@
 package ua.com.owu.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -21,18 +22,38 @@ import java.util.Properties;
 public class DataConfig {
 
 
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost/springmvc1500");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-        return dataSource;
-    }
+
+       @Bean
+       public DataSource dataSource(){
+   //        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+   //        dataSource.setUrl("jdbc:mysql://localhost/springmvc2015");
+   //        dataSource.setUsername("root");
+   //        dataSource.setPassword("root");
+   //        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+   //        return dataSource;
+           HikariConfig hikariConfig = new HikariConfig();
+           hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+           hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/springmvc2015");
+           hikariConfig.setUsername("root");
+           hikariConfig.setPassword("root");
+
+           hikariConfig.setMaximumPoolSize(5);
+           hikariConfig.setConnectionTestQuery("SELECT 1");
+           hikariConfig.setPoolName("springHikariCP");
+
+           hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
+           hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "250");
+           hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
+           hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
+
+           HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+           return dataSource;
+       }
+
 
     @Bean
-    public JpaVendorAdapter vendorAdapter(){
+    public JpaVendorAdapter vendorAdapter() {
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         vendorAdapter.setDatabase(Database.MYSQL);
         vendorAdapter.setShowSql(true);
@@ -40,7 +61,7 @@ public class DataConfig {
     }
 
     @Bean("entityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         factoryBean.setDataSource(dataSource());
         factoryBean.setJpaVendorAdapter(vendorAdapter());
